9c4d6f6f396755fb8249e683b6078856266ebe83
jkaunert <44586402+jkaunert@users.noreply.github.com44586402+jkaunert@users.noreply.github.com44586402+jkaunert@users.noreply.github.com44586402+jkaunert@users.noreply.github.com>
2019-07-24 09:59:12 -0500

    refactored XCTests

--- ExponentialGrowthObjC/ExponentialGrowthObjCTests/ExponentialGrowthObjCTests.m
+++ ExponentialGrowthObjC/ExponentialGrowthObjCTests/ExponentialGrowthObjCTests.m
@@ -7,7 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
-#import "ExponentialGrowthObjC/GrainsOfRice.h"
+#import "GrainsOfRice.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -21,67 +21,67 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)testNumberOfGrainsOnSquare1 {
     long long expected = 1;
-    long long result = [[[Grains alloc]init]grainsOnSquareNumber:1];
+    long long result = [[[GrainsOfRice alloc]init]grainsOnSquareNumber:1];
     XCTAssertEqual(expected,result);
 }
 
 - (void)testNumberOfGrainsOnSquare2 {
     long long expected = 2;
-    long long result = [[[Grains alloc]init]grainsOnSquareNumber:2];
+    long long result = [[[GrainsOfRice alloc]init]grainsOnSquareNumber:2];
     XCTAssertEqual(expected,result);
 }
 
 - (void)testNumberOfGrainsOnSquare3 {
     unsigned long long expected = 4;
-    unsigned long long result = [[[Grains alloc]init]grainsOnSquareNumber:3];
+    unsigned long long result = [[[GrainsOfRice alloc]init]grainsOnSquareNumber:3];
     XCTAssertEqual(expected,result);
 }
 
 - (void)testNumberOfGrainsOnSquare4 {
     unsigned long long expected = 8;
-    unsigned long long result = [[[Grains alloc]init]grainsOnSquareNumber:4];
+    unsigned long long result = [[[GrainsOfRice alloc]init]grainsOnSquareNumber:4];
     XCTAssertEqual(expected,result);
 }
 
 - (void)testNumberOfGrainsOnSquare16 {
     unsigned long long expected = 32768;
-    unsigned long long result = [[[Grains alloc]init]grainsOnSquareNumber:16];
+    unsigned long long result = [[[GrainsOfRice alloc]init]grainsOnSquareNumber:16];
     XCTAssertEqual(expected,result);
 }
 
 - (void)testNumberOfGrainsOnSquare32 {
     unsigned long long expected = 2147483648;
-    unsigned long long result = [[[Grains alloc]init]grainsOnSquareNumber:32];
+    unsigned long long result = [[[GrainsOfRice alloc]init]grainsOnSquareNumber:32];
     XCTAssertEqual(expected,result);
 }
 
 - (void)testNumberOfGrainsOnSquare64 {
     unsigned long long expected = 9223372036854775808;
-    unsigned long long result = [[[Grains alloc]init]grainsOnSquareNumber:64];
+    unsigned long long result = [[[GrainsOfRice alloc]init]grainsOnSquareNumber:64];
     XCTAssertEqual(expected,result);
 }
 
 - (void)testNumberOfGrainsOnSquare65 {
     unsigned long long expected = -1;
-    unsigned long long result = [[[Grains alloc]init]grainsOnSquareNumber:65];
+    unsigned long long result = [[[GrainsOfRice alloc]init]grainsOnSquareNumber:65];
     XCTAssertEqual(expected,result);
 }
 
 - (void)testNumberOfGrainsOnSquareNeg1 {
     unsigned long long expected = -1;
-    unsigned long long result = [[[Grains alloc]init]grainsOnSquareNumber:-1];
+    unsigned long long result = [[[GrainsOfRice alloc]init]grainsOnSquareNumber:-1];
     XCTAssertEqual(expected,result);
 }
 
 - (void)testNumberOfGrainsOnSquare0 {
     unsigned long long expected = -1;
-    unsigned long long result = [[[Grains alloc]init]grainsOnSquareNumber:0];
+    unsigned long long result = [[[GrainsOfRice alloc]init]grainsOnSquareNumber:0];
     XCTAssertEqual(expected,result);
 }
 
 - (void)testTotalNumberOfGrains {
     unsigned long long expected = 18446744073709551615;
-    unsigned long long result = [[[Grains alloc]init]grainsOnBoard];
+    unsigned long long result = [[[GrainsOfRice alloc]init]grainsOnBoard];
     XCTAssertEqual(expected,result);
 }
 
